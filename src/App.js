0f97bde016c5dbe6e743874a28892465479f45d6
MohamadFawas <mohamadfawas19971003@gmail.com>
2024-04-20 16:37:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './component/Layouts/Index'
+import Home from './component/Home/Index'
+
+export default function App() {
+  return (
+    <Layout>
+      <Home/>
+    </Layout>
+  )
+}
+
